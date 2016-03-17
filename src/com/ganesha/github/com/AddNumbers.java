e12c9cfbef666bf9d4c02298a4701ee8fd71a7c8
msingh <msingh@MSINGH-PC2.prosrm.com>
2016-03-17 16:16:06 -0500

    Initial version of java code to add 2 no's

---
+++ src/com/ganesha/github/com/AddNumbers.java
@@ -0,0 +1,16 @@
+package com.ganesha.github.com;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 5 ,b = 10, sum = 0;
+		sum = a + b;
+		System.out.println(sum);
+		System.out.println(sum);
+
+	}
+
+}
